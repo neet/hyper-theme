ea2b7c9cc6cb74a80a8a6f5cee2d72f8a62ac2ee
neet <n33t5hin@gmail.com>
2018-07-12 01:11:55 +0900

    Improve CSS

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const syntaxHue           = '220';
-const syntaxSubsituabtion = '13%';
-const syntaxBrightness    = '18%';
-
 // Monochrome
 const mono = {
-  '1': `hsl(${syntaxHue}, 14%, 71%)`,
-  '2': `hsl(${syntaxHue}, 9%, 55%)`,
-  '3': `hsl(${syntaxHue}, 10%, 40%)`,
+  '1': `hsl(220, 14%, 71%)`,
+  '2': `hsl(220, 9%, 55%)`,
+  '3': `hsl(220, 10%, 40%)`,
 };
 
 // Colors
@@ -26,11 +22,16 @@ const hue = {
 };
 
 // Base color
+const uiFg         = 'hsl(220, 13%, 18%)';
+const uiBg         = 'hsl(216, 13%, 15%)';
+const uiBgDarker   = 'hsl(223, 13%, 11%)';
+const uiAccent     = 'hsl(220, 86%, 64%)';
+
 const syntaxFg     = mono['1'];
-const syntaxBg     = `hsl(${syntaxHue}, ${syntaxSubsituabtion}, ${syntaxBrightness})`;
-const syntaxGutter = `hsl(${syntaxHue}, 14%, 89%)`;
-const syntaxGuide  = `hsl(${syntaxHue}, 16%, 71%)`;
-const syntaxAccent = `hsl(${syntaxHue}, 100%, 66%)`;
+const syntaxBg     = 'hsl(220, 13%, 18%)';
+const syntaxGutter = 'hsl(220, 14%, 45%)';
+const syntaxGuide  = 'rgba(171, 178, 191, 0.15)';
+const syntaxAccent = 'rgba(82, 139, 255, 0.24)';
 
 
 module.exports.decorateConfig = (config) => {
@@ -43,6 +44,7 @@ module.exports.decorateConfig = (config) => {
     fontSize:        16,
     fontWeight:      500,
     fontWeightBold:  700,
+    selectionColor:  syntaxGuide,
     backgroundColor: syntaxBg,
     foregroundColor: syntaxFg,
     borderColor:     syntaxGutter,
@@ -69,122 +71,59 @@ module.exports.decorateConfig = (config) => {
 
     css: `
       ${config.css || ''}
-      .header_header {
-        top: 0;
-        right: 0;
-        left: 0;
-      }
-      .tabs_list {
-        background-color: #21252b !important;
-        border-bottom-color: #181a1f !important;
-      }
-      .splitpane_divider {
-        background-color: rgba(171, 178, 191, 0.15) !important;
-      }
+
       .tab_tab {
-        font-weight: 500;
-        color: rgba(157, 165, 180, 0.6);
-        border-width: 0 0 0 1px;
-        border-image: linear-gradient(#21252b, #181a1f 1em) 0 0 0 1 stretch;
-        border-style: solid;
+        color: ${mono['2']} !important;
+        background-color: ${uiBg} !important;
       }
-      .tab_tab:first-of-type {
-        border-width: 0;
-      }
-      .tab_tab:hover {
-        color: rgba(157, 165, 180, 0.6);
-        transition: none;
-      }
-      .tab_tab::after {
-        content: "";
-        position: absolute;
-        pointer-events: none;
-        top: 0;
-        bottom: -1px;
-        left: 0;
-        width: 2px;
-        height: inherit;
-        background: #528bff;
-        opacity: 0;
-        transition: opacity .16s;
-        z-index: 1;
-      }
+
       .tabs_title,
       .tab_tab.tab_active {
-        font-weight: 500;
-        color: #d7dae0;
+        color: ${mono['1']} !important;
+        background-color: ${uiFg} !important;
+        box-shadow: 0px 1px 0px ${uiFg} !important;
       }
-      .tab_tab.tab_active {
-        background-color: ${syntaxBg};
-      }
-      .tab_tab.tab_active,
-      .tab_tab.tab_active + .tab_tab {
-        border-image: linear-gradient(transparent, transparent) 0 0 0 1 stretch;
-      }
-      .tab_tab.tab_active::before {
-        content: "";
-        z-index: 1;
-        position: absolute;
-        top: 0;
-        left: -1px;
-        bottom: -1px;
-        right: 0;
-        height: inherit;
-        background-image: linear-gradient(rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0));
-        box-shadow: inset 0 1px 1px rgba(255, 255, 255, 0.06);
-        border: 1px solid #181a1f;
-        border-bottom-color: ${syntaxBg};
-        border-top: 0;
-      }
-      .tab_tab.tab_active:last-of-type::before {
-        border-right-width: 0;
-      }
-      .tab_tab.tab_active::after {
-        opacity: 1;
-        transition-duration: .32s;
-      }
+
       .tab_icon {
-        display: block;
-        background: rgba(157, 165, 180, 0.6);
-        -webkit-mask-image: url('${__dirname}/close.svg');
-        mask-image: url('${__dirname}/close.svg');
-        -webkit-mask-size: 7px;
-        mask-size: 7px;
-        -webkit-mask-repeat: no-repeat;
-        mask-repeat: no-repeat;
-        -webkit-mask-position-y: center;
-        mask-position-y: center;
-        -webkit-mask-position-x: 8px;
-        mask-position-x: 8px;
-        width: 26px;
-        height: 100%;
-        top: 0;
-        right: 0;
-        transform: scale(0);
-        transition: transform .08s;
-        opacity: 1;
-        border-radius: 0;
-        z-index: 2;
+        background-color: ${uiBgDarker} !important;
       }
+
       .tab_icon:hover {
-        background: rgba(157, 165, 180, 0.6);
-        opacity: .7;
+        background-color: ${uiAccent} !important;
       }
-      .tab_tab.tab_active .tab_icon {
-        background: #d7dae0;
+
+      .tabs_list {
+        border-color: ${uiBgDarker} !important;
       }
-      .tab_tab.tab_active .tab_icon:hover {
-        background: #d7dae0;
+
+      .footer_footer {
+        opacity: 1;
+        background-color: ${uiBg};
+        border-top: 1px solid ${uiBgDarker};
       }
-      .tab_tab:hover .tab_icon {
-        transform: scale(1);
-        transition-duration: .16s;
+
+      .footer_footer .item_clickable:hover {
+        text-decoration: none;
       }
-      .tab_icon svg {
-        display: none;
+
+      .footer_footer .footer_group {
+        color: ${mono['2']} !important;
+        transition: background 150ms ease;
       }
-      .tab_icon::after {
-        display: none;
+
+      .footer_footer .item_icon:before {
+        background-color: ${mono['2']} !important;
+        transition: background 150ms ease;
+      }
+
+      .footer_footer:hover .footer_group {
+        color: ${mono['1']} !important;
+        transition: background 150ms ease;
+      }
+
+      .footer_footer:hover .item_icon:before {
+        background-color: ${mono['1']} !important;
+        transition: background 150ms ease;
       }
     `,
   }, userConfig);
