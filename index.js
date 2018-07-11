bf9f3cf8ffba8661fcbbf0648dc26b0b3ac9de66
neet <n33t5hin@gmail.com>
2018-07-11 23:40:20 +0900

    何をしたんだ

--- index.js
+++ index.js
@@ -32,11 +32,9 @@ const syntaxGutter = `hsl(${syntaxHue}, 14%, 89%)`;
 const syntaxGuide  = `hsl(${syntaxHue}, 16%, 71%)`;
 const syntaxAccent = `hsl(${syntaxHue}, 100%, 66%)`;
 
-module.exports.onWindow = (browserWindow) => (
-  browserWindow.setVibrancy("ultra-dark")
-);
+module.exports.onWindow = (browserWindow) => browserWindow.setVibrancy("ultra-dark");
 
-module.exports.decorateConfig = (config) => {
+exports.decorateConfig = (config) => {
   // The key which will be used in `~/.hyper.js`
   const configKey  = 'theme';
   const userConfig = config.hasOwnProperty(configKey) && config[configKey];
