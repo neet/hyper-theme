c638171e741756832410528de21df5add7a765c9
neet <n33t5hin@gmail.com>
2018-08-23 02:52:29 +0900

    Update color scheme

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-// Monochrome
-const mono = {
-  '1': 'hsl(220, 14%, 71%)',
-  '2': 'hsl(220, 9%, 55%)',
-  '3': 'hsl(220, 10%, 40%)',
-};
+const backgroundColor = '#292D3E';
+const foregroundColor = '#959DCB';
+const cursorColor     = '#FFCC00';
+const selectionColor  = '#6a5e32';
+const borderColor     = backgroundColor;
 
-// Colors
-const hue = {
-  '1': 'hsl(187, 47%, 55%)', // cyan
-  '2': 'hsl(207, 82%, 66%)', // blue
-  '3': 'hsl(286, 60%, 67%)', // purple
-  '4': 'hsl( 95, 38%, 62%)', // green
-  '5': {
-    '1': 'hsl(355, 65%, 65%)', // red 1
-    '2': 'hsl( 5, 48%, 51%)',  // red 2
-  },
-  '6': {
-    '1': 'hsl( 29, 54%, 61%)', // orange 1
-    '2': 'hsl( 39, 67%, 69%)', // orange 2
-  },
-};
-
-// Base color
-const uiFg         = 'hsl(220, 13%, 18%)';
-const uiBg         = 'hsl(216, 13%, 15%)';
-const uiBgDarker   = 'hsl(223, 13%, 11%)';
-const uiAccent     = 'hsl(220, 86%, 64%)';
-
-const syntaxFg     = mono['1'];
-const syntaxBg     = 'hsl(220, 13%, 18%)';
-const syntaxGutter = 'hsl(220, 14%, 45%)';
-const syntaxGuide  = 'rgba(171, 178, 191, 0.15)';
-// const syntaxAccent = 'rgba(82, 139, 255, 0.24)';
+const colors = {
+	black        : backgroundColor,
+	red          : '#F07178',
+	green  	     : '#C3E88D',
+	yellow       : '#FFCB6B',
+	blue         : '#82AAFF',
+	magenta      : '#C792EA',
+	cyan         : '#89DDFF',
+	white        : '#d0d0d0',
+	lightBlack   : '#434758',
+	lightRed     : '#FF8B92',
+	lightGreen   : '#DDFFA7',
+	lightYellow  : '#FFE585',
+	lightBlue    : '#9CC4FF',
+	lightMagenta : '#E1ACFF',
+	lightCyan    : '#A3F7FF',
+	colorCubes   : '#ffffff',
+	grayscale    : foregroundColor,
+}
 
 
 module.exports.decorateConfig = (config) => {
@@ -44,30 +35,12 @@ module.exports.decorateConfig = (config) => {
     fontSize:        16,
     fontWeight:      500,
     fontWeightBold:  700,
-    selectionColor:  syntaxGuide,
-    backgroundColor: syntaxBg,
-    foregroundColor: syntaxFg,
-    borderColor:     syntaxGutter,
-    cursorColor:     mono['2'],
-
-    colors: {
-      black:        mono['2'],
-      red:          hue['5']['1'],
-      green:        hue['4'],
-      yellow:       hue['6']['1'],
-      blue:         hue['2'],
-      magenta:      hue['3'],
-      cyan:         hue['1'],
-      white:        mono['1'],
-      lightBlack:   mono['2'],
-      lightRed:     hue['5']['2'],
-      lightGreen:   hue['4'],
-      lightYellow:  hue['6']['2'],
-      lightBlue:    hue['2'],
-      lightMagenta: hue['3'],
-      lightCyan:    hue['1'],
-      lightWhite:   mono['1'],
-    },
+    selectionColor,
+    backgroundColor,
+    foregroundColor,
+    borderColor,
+    cursorColor,
+    colors,
 
     css: `
       ${config.css || ''}
